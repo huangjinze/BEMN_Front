91d7330928d358698d4448fd8d7de478b9053f02
suchang <1103232282@qq.com>
2017-11-14 15:45:42 +0800

    element init

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
+import App from './App.vue'
 import router from './router/index'
+import {Tag} from 'element-ui'
 
 Vue.config.productionTip = false
 
+Vue.use(Tag)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
