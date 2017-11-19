298ea7a730c0b5c6449f8f1565bb261b86cbab07
luminyi <luminyi@bjfu.edu.cn>
2017-11-19 22:28:49 +0800

    add

---
+++ src/components/counter.vue
@@ -0,0 +1,18 @@
+<template>
+    <el-input-number v-model="num1" @change="handleChange" :min="1995" :max="2020" label="描述文字"></el-input-number>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                num1: 2016
+            };
+        },
+        methods: {
+            handleChange(value) {
+                console.log(value);
+            }
+        }
+    };
+</script>
