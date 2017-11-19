9ba3eebecd40b703533509536da877646fb3be6f
suchang <1103232282@qq.com>
2017-11-19 20:35:35 +0800

    static

---
+++ src/page/StaiscticPage.vue
@@ -0,0 +1,47 @@
+<<template>
+ <el-container>
+  <el-header>Header</el-header>
+  <el-container>
+    <el-aside width="200px">{{this.$data.formValue}}</el-aside>
+    <el-main>
+        <div class="guider">
+        </div>
+        <charts class="testchart" id="1" :targetOptions="targetOptions" :typeOptions="typeOptions" @ValueChange="ValueChange"></charts>
+    </el-main>
+  </el-container>
+</el-container>
+</template>
+
+<<script>
+
+import charts from '../components/echart/charts.vue'
+
+export default {
+  components: {charts},
+  name: 'StaiscticPage',
+  data () {
+    return {
+      targetOptions: [
+        {value: '1', label: '1'},
+        {value: '2', label: '2'},
+        {value: '3', label: '3'},
+        {value: '4', label: '4'}],
+      typeOptions: [
+        {value: 'a', label: 'a'},
+        {value: 'b', label: 'b'}],
+      formValue: {}
+    }
+  },
+  methods: {
+    ValueChange (value) {
+      this.$data.formValue = value
+      console.log('on su')
+    }
+  }
+
+}
+</script>
+
+<style scoped>
+
+</style>
