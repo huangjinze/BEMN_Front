1550071bd24f20ee6e84d1106d50386482a5f5b6
chenyujie <m13121163355@163.com>
2017-11-26 11:35:05 +0800

    add 分页表格

---
+++ src/components/dataManager.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="dataManager">
+    <el-tabs type="border-card">
+      <el-tab-pane label="森林群落结构">
+        
+      </el-tab-pane>
+      <el-tab-pane label="森林群落乔木层生物量和林木生长量及养分">
+        配置管理
+      </el-tab-pane>
+      <el-tab-pane label="森林凋落物量">角色管理
+
+      </el-tab-pane>
+      <el-tab-pane label="群落的天然更新">定时任务补偿
+        
+      </el-tab-pane>
+    </el-tabs>
+  </div>
+</template>
--- src/page/HomePage.vue
+++ src/page/HomePage.vue
@@ -1,17 +1,17 @@
-<<template>
+<template>
 <BasePage>
   <div slot="header">header</div>
   <div slot="aside"><navi></navi></div>
-  <div slot="main">Hello!!BEMN</div>
+  <div slot="main"><dataManager></dataManager></div>
 </BasePage>
 </template>
 
-<<script>
+<script>
   import navi from '../components/layout/navi'
   import BasePage from '../components/BasePage'
-
+  import dataManager from '../components/dataManager'
   export default {
-    components: {navi, BasePage}
+    components: {navi, BasePage, dataManager}
   }
 </script>
 
