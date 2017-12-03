d8a84aea18717f6bb59d0766030858e7c18c729c
chenyujie <m13121163355@163.com>
2017-12-03 13:20:55 +0800

    git merge

--- src/page/User/LoginPage.vue
+++ src/page/User/LoginPage.vue
@@ -14,16 +14,17 @@
       return {
         loginForm: {
         },
-        codeUrl: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1512470460&di=66d2f289cb3b77b41dc97e91842da2d4&imgtype=jpg&er=1&src=http%3A%2F%2Fwenwen.soso.com%2Fp%2F20101026%2F20101026151436-1771937959.jpg'
+        codeUrl: 'http://172.19.32.116/captcha/mews?r=' + Math.random()
       }
     },
     methods: {
       onRefreshCode () {
         console.log('refresh code')
+        this.codeUrl = 'http://172.19.32.116/captcha/mews?r=' + Math.random()
       },
       onLoginClick () {
         console.log('on login')
-        this.LoginUser()
+        this.LoginUser(this.loginForm)
       },
       ...mapActions({LoginUser: 'Login'})
     }
