2ccd0bb52f92fa42571ad79ea3e69ec7ded434c3
luminyi <luminyi@bjfu.edu.cn>
2017-12-10 22:38:23 +0800

    完成login界面背景

--- src/page/User/LoginPage.vue
+++ src/page/User/LoginPage.vue
@@ -1,11 +1,34 @@
-<<template>
-
-  <login v-model="loginForm" :CodeUrl="codeUrl" @refreshCode="onRefreshCode" @loginClick="onLoginClick"></login>
+<template>
+  <div>
+    <div ref="mybox" style="position:absolute;left:0;top:0"></div>
+    <login v-model="loginForm" :CodeUrl="codeUrl" @refreshCode="onRefreshCode" @loginClick="onLoginClick"></login>
+  </div>
 </template>
 
-<<script>
+<script>
   import login from '../../components/user/login.vue'
   import {mapActions} from 'vuex'
+  import * as THREE from 'three'
+  import nx from '../../assets/pages/textures/nx.jpg'
+  import ny from '../../assets/pages/textures/ny.jpg'
+  import nz from '../../assets/pages/textures/nz.jpg'
+  import px from '../../assets/pages/textures/px.jpg'
+  import py from '../../assets/pages/textures/py.jpg'
+  import pz from '../../assets/pages/textures/pz.jpg'
+  var camera
+  var scene
+  var renderer
+  var textureplaceholder
+  var isUserInteracting = false
+  var lon = 90
+  var lat = 0
+  var phi = 0
+  var theta = 0
+  var onPointerDownPointerX
+  var onPointerDownPointerY
+  var onPointerDownLon
+  var onPointerDownLat
+  var target = new THREE.Vector3()
 
   export default {
     name: 'LoginPage',
@@ -17,6 +40,10 @@
         codeUrl: 'http://172.19.32.116/captcha/mews?r=' + Math.random()
       }
     },
+    mounted: function () {
+      this.init()
+      this.animate()
+    },
     methods: {
       onRefreshCode () {
         console.log('refresh code')
@@ -26,7 +53,136 @@
         console.log('on login')
         this.LoginUser(this.loginForm)
       },
-      ...mapActions({LoginUser: 'Login'})
+      ...mapActions({LoginUser: 'Login'}),
+      init () {
+        var container, mesh
+        container = this.$refs.mybox
+//        alert(container)
+        console.log(container)
+        camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 1100)
+        scene = new THREE.Scene()
+        textureplaceholder = document.createElement('canvas')
+        textureplaceholder.width = 128
+        textureplaceholder.height = 128
+        var context = textureplaceholder.getContext('2d')
+        context.fillStyle = 'rgb( 200, 200, 200 )'
+        context.fillRect(0, 0, textureplaceholder.width, textureplaceholder.height)
+        var materials = [
+          this.loadTexture(px), // right
+          this.loadTexture(nx), // left
+          this.loadTexture(py), // top
+          this.loadTexture(ny), // bottom
+          this.loadTexture(pz), // back
+          this.loadTexture(nz) // front
+        ]
+        mesh = new THREE.Mesh(new THREE.BoxGeometry(300, 300, 300, 7, 7, 7), new THREE.MeshFaceMaterial(materials))
+        mesh.scale.x = -1
+        scene.add(mesh)
+        for (var i = 0, l = mesh.geometry.vertices.length; i < l; i++) {
+          var vertex = mesh.geometry.vertices[ i ]
+          vertex.normalize()
+          vertex.multiplyScalar(550)
+        }
+//        renderer = new THREE.CanvasRenderer()
+        renderer = new THREE.WebGLRenderer({antialias: true})
+        console.log(renderer)
+        renderer.setPixelRatio(window.devicePixelRatio)
+        renderer.setSize(window.innerWidth, window.innerHeight)
+        container.appendChild(renderer.domElement)
+//        alert(container.appendChild(renderer.domElement))
+        document.addEventListener('mousedown', this.onDocumentMouseDown, false)
+        document.addEventListener('mousemove', this.onDocumentMouseMove, false)
+        document.addEventListener('mouseup', this.onDocumentMouseUp, false)
+        document.addEventListener('mousewheel', this.onDocumentMouseWheel, false)
+        document.addEventListener('MozMousePixelScroll', this.onDocumentMouseWheel, false)
+        document.addEventListener('touchstart', this.onDocumentTouchStart, false)
+        document.addEventListener('touchmove', this.onDocumentTouchMove, false)
+        //
+        window.addEventListener('resize', this.onWindowResize, false)
+      },
+      onWindowResize () {
+        camera.aspect = window.innerWidth / window.innerHeight
+        camera.updateProjectionMatrix()
+        renderer.setSize(window.innerWidth, window.innerHeight)
+      },
+      loadTexture (path) {
+        var texture = new THREE.Texture(textureplaceholder)
+        var material = new THREE.MeshBasicMaterial({ map: texture, overdraw: 0.5 })
+        var image = new Image()
+        image.onload = function () {
+          texture.image = this
+          texture.needsUpdate = true
+        }
+        image.src = path
+        return material
+      },
+      onDocumentMouseDown (event) {
+        // event.preventDefault();
+        isUserInteracting = true
+        onPointerDownPointerX = event.clientX
+        onPointerDownPointerY = event.clientY
+        onPointerDownLon = lon
+        onPointerDownLat = lat
+      },
+      onDocumentMouseMove (event) {
+        if (isUserInteracting === true) {
+          lon = (onPointerDownPointerX - event.clientX) * 0.1 + onPointerDownLon
+          lat = (event.clientY - onPointerDownPointerY) * 0.1 + onPointerDownLat
+        }
+      },
+      onDocumentMouseUp (event) {
+        isUserInteracting = false
+      },
+      onDocumentMouseWheel (event) {
+        // WebKit
+        if (event.wheelDeltaY) {
+          camera.fov -= event.wheelDeltaY * 0.05
+          // Opera / Explorer 9
+        } else if (event.wheelDelta) {
+          camera.fov -= event.wheelDelta * 0.05
+          // Firefox
+        } else if (event.detail) {
+          camera.fov -= event.detail * 0.05
+        }
+        camera.updateProjectionMatrix()
+      },
+      onDocumentTouchStart (event) {
+        if (event.touches.length === 1) {
+          event.preventDefault()
+          onPointerDownPointerX = event.touches[ 0 ].pageX
+          onPointerDownPointerY = event.touches[ 0 ].pageY
+          onPointerDownLon = lon
+          onPointerDownLat = lat
+        }
+      },
+      onDocumentTouchMove (event) {
+        if (event.touches.length === 1) {
+          event.preventDefault()
+          lon = (onPointerDownPointerX - event.touches[0].pageX) * 0.1 + onPointerDownLon
+          lat = (event.touches[0].pageY - onPointerDownPointerY) * 0.1 + onPointerDownLat
+        }
+      },
+      animate () {
+        requestAnimationFrame(this.animate)
+        this.update()
+      },
+      update () {
+        if (isUserInteracting === false) {
+          lon += 0.1
+        }
+        lat = Math.max(-85, Math.min(85, lat))
+        phi = THREE.Math.degToRad(90 - lat)
+        theta = THREE.Math.degToRad(lon)
+
+        target.x = 500 * Math.sin(phi) * Math.cos(theta)
+        target.y = 500 * Math.cos(phi)
+        target.z = 500 * Math.sin(phi) * Math.sin(theta)
+
+        camera.position.copy(target).negate()
+        camera.lookAt(target)
+
+        renderer.render(scene, camera)
+      }
     }
   }
 </script>
