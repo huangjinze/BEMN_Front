131e0b3cc1aeb9c07549e633210238630003c7f2
suchang <110323282@qq.com>
2018-01-06 13:54:02 +0800

    change ignore

--- src/components/datawashing/rangeCheck.vue
+++ src/components/datawashing/rangeCheck.vue
@@ -2,8 +2,8 @@
   <el-form>
     <el-form-item v-for="(item,index) in indexes" :key="index">
       <span>{{ item.name }}:
-        <el-input-number v-model="form[index].low"></el-input-number> -
-        <el-input-number v-model="form[index].high"></el-input-number> </span>
+        <el-input-number v-model="item.low"></el-input-number> -
+        <el-input-number v-model="item.high"></el-input-number> </span>
     </el-form-item>
   </el-form>
 </template>
@@ -14,26 +14,8 @@
   export default {
     components: {},
     name: 'rangeCheck',
-    data () {
-      return {
-        form: []
-      }
-    },
     props: {
-      indexes: {type: Array,
-        default: function () {
-          return [
-          { name: 'H', low: 255, high: 540},
-          { name: 'co2_flux', low: 200, high: 500},
-          { name: 'h2o_flux', low: 200, high: 500}]
-        }
-      }
-    },
-    created: function () {
-      for (let item in this.indexes) {
-        console.log(this.indexes[item])
-        this.form.push({name: this.indexes[item].name, low: this.indexes[item].low, high: this.indexes[item].high})
-      }
+      indexes: {type: Array}
     }
   }
 </script>
--- src/model/data.js
+++ src/model/data.js
@@ -6,3 +6,9 @@ export const getVFTIndex = params => { return axios.get(host + '/station/vft/get
 export const getVTFData = params => { return axios.get(host + '/fluxdata/statsdata/show', {'params': params}) }
 export const getDataManager = params => { return axios.get(host + 'http://172.19.32.116/fluxdata/origindata/show', {'params': params}) }
 export const getIndexData = params => { return axios.get(host + 'http://172.19.32.116/fluxdata/origindata/show', {'params': params}) }
+
+// washing
+
+export const getWashingIndexAndRange = params => {
+  return axios.get(host + '/station/vft/getIndexesRange', {'params': params})
+}
