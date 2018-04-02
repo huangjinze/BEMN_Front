87668c3fbea81aa252ddb99cf6322e52f9e512df
Chang Su <suchang@gmail.com>
2018-04-02 23:12:51 +0800

    fix nav

--- src/components/BasePage.vue
+++ src/components/BasePage.vue
@@ -1,35 +1,36 @@
 
 <template>
+
   <el-container>
-    <!--北京城市副中心核心区林地绿地大数据平台-->
-    <el-header>
-      <slot name="header">北京城市副中心核心区林地绿地大数据平台</slot>
-      <el-dropdown style="float: right">
-        <el-button type="text"  style="font-size: 16px;color: white">
-          欢迎您！{{ msg[0][0].name }}<i class="el-icon-arrow-down el-icon--right"></i>
-        </el-button>
-        <el-dropdown-menu slot="dropdown">
-          <el-dropdown-item><el-button type="text" @click="info">个人信息</el-button></el-dropdown-item>
-          <el-dropdown-item><el-button type="text" @click="logout">退出</el-button></el-dropdown-item>
-        </el-dropdown-menu>
-      </el-dropdown>
-    </el-header>
+    <el-aside width="180px" style="background-color: #314254"><navi></navi></el-aside>
     <el-container>
-      <el-aside width="180px" style="background-color: #314254"><navi></navi></el-aside>
+      <el-header>
+        <slot name="header">北京城市副中心核心区林地绿地大数据平台</slot>
+        <el-dropdown style="float: right">
+          <el-button type="text"  style="font-size: 16px;color: white">
+            欢迎您！{{ msg[0][0].name }}<i class="el-icon-arrow-down el-icon--right"></i>
+          </el-button>
+          <el-dropdown-menu slot="dropdown">
+            <el-dropdown-item><el-button type="text" @click="info">个人信息</el-button></el-dropdown-item>
+            <el-dropdown-item><el-button type="text" @click="logout">退出</el-button></el-dropdown-item>
+          </el-dropdown-menu>
+        </el-dropdown>
+      </el-header>
       <el-main><slot name="main"></slot></el-main>
+      <el-footer>Footer</el-footer>
     </el-container>
-    <el-footer>
-      <slot name="footer"></slot>
-      <slot name="footer-copyright"></slot>
-    </el-footer>
   </el-container>
 
 </template>
 <script>
   import {mapGetters} from 'vuex'
   import {logoutUser} from '../model/user'
-  export default {
+  import navi from './layout/navi'
 
+  export default {
+    components: {
+      navi
+    },
     created: function () {
 
     },
@@ -67,7 +68,7 @@
     background-color: #fff;
     height: 75px!important;
     text-align: center;
-    color: #fff;
+    color: #000000;
     font-size: 24px;
   }
   .el-footer{
