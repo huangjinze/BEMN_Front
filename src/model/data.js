1602340919c9fda15ae125ec0cf330804d4549fc
luminyi <luminyi@bjfu.edu.cn>
2018-04-03 21:53:16 +0800

    改bug

--- src/model/data.js
+++ src/model/data.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
-// export const host = 'http://222.31.175.157:10080'
-export const host = 'http://bemnwork'
+export const host = 'http://222.31.175.157:10080'
+// export const host = 'http://bemnwork'
 export const getVFTIndex = params => { return axios.get(host + '/station/vft/getIndexes', {'params': params}) }
 export const getVTFData = params => { return axios.get(host + '/fluxdata/statsdata/show', {'params': params}) }
 export const getDataManager = params => { return axios.get(host + '/fluxdata/origindata/show', {'params': params}) }
