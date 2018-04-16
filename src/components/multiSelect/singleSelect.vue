fd27bb8adf3ab459d0cd371bcbfad4d08a15d918
luminyi <luminyi@bjfu.edu.cn>
2018-04-16 20:34:04 +0800

    改bug

--- src/components/multiSelect/singleSelect.vue
+++ src/components/multiSelect/singleSelect.vue
@@ -24,13 +24,19 @@ export default {
   },
   mounted: function () {
     getStation({domain: '通量数据'}).then(resp => {
-      let data = resp.data.data
-      this.index.splice(0, this.index.length)
-      this.index.push({ text: '选择站点', flag: 1 })
-      for (var i = 0; i < data.length; i++) {
-        this.indexTags.push({ text: data[i], id: i + 1 })
+      if (resp.data.status === 'fail') {
+        this.$alert('抱歉，您暂时没有管理的站点！', {confirmButtonText: 'ok'})
+        // this.stationName = ['']
+        // this.className = ['']
+      } else {
+        let data = resp.data.data
+        this.index.splice(0, this.index.length)
+        this.index.push({ text: '选择站点', flag: 1 })
+        for (var i = 0; i < data.length; i++) {
+          this.indexTags.push({ text: data[i], id: i + 1 })
+        }
+        this.stations = Array.from(this.indexTags)
       }
-      this.stations = Array.from(this.indexTags)
     }).catch(resp => {
       this.$alert('获取失败', '失败', {confirmButtonText: 'ok'})
     })
